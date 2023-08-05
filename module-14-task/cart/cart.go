f4f6cc9eb7a177f1dae7ce92a8675715fa5f9bcd
blckvia <a.vyatkinn@icloud.com>
2023-08-05 19:40:58 +0300

    module 14 task solution has been added

---
+++ module-14-task/cart/cart.go
@@ -0,0 +1,87 @@
+package cart
+
+import (
+	"os/user"
+	"test/product"
+	"testing"
+	"time"
+
+	"github.com/Rhymond/go-money"
+	"github.com/stretchr/testify/assert"
+)
+
+type Cart struct {
+    ID        string
+    CreatedAt time.Time
+    UpdatedAt time.Time
+    lockedAt  time.Time
+    user.User
+    Items        []Item
+    CurrencyCode string
+    isLocked     bool
+}
+
+type Item struct {
+    product.Product
+    Quantity uint8
+}
+
+func TestTotalPrice(t *testing.T) {
+    items := []Item{
+        {
+            Product: product.Product{
+                ID:    "p-1254",
+                Name:  "Product test",
+                Price: money.New(1000, "EUR"),
+            },
+            Quantity: 2,
+        },
+        {
+            Product: product.Product{
+                ID:    "p-1255",
+                Name:  "Product test 2",
+                Price: money.New(2000, "EUR"),
+            },
+            Quantity: 1,
+        },
+    }
+
+    c := Cart{
+        ID:        "123",
+        CreatedAt: time.Now(),
+        UpdatedAt: time.Now(),
+        User: user.User{},
+        Items: items,
+        CurrencyCode: "EUR",
+    }
+
+    actual, err := c.TotalPrice()
+    assert.NoError(t, err)
+    assert.Equal(t, money.New(4000, "EUR"), actual)
+}
+
+func (c *Cart) TotalPrice() (*money.Money, error) {
+    total := money.New(0, c.CurrencyCode)
+    // var err error
+    for _, v := range c.Items {
+        itemSubtotal := v.Product.Price.Multiply(int64(v.Quantity))
+        total, err := total.Add(itemSubtotal)
+        if err != nil {
+            return nil, err
+        }
+        
+    return total, nil
+    }
+
+    return nil, nil
+}
+
+func (c *Cart) Lock() error {
+    //...
+    return nil
+}
+
+func (c *Cart) delete() error {
+    // to implement
+    return nil
+}
